5ee388d443d0d3bf27c341c9ae0412c75dff665a
Yasuki <yasuki.wu.02@gmail.com>
2019-05-27 22:53:22 -0500

    test commit

--- src/froggermain/FroggerMain.java
+++ src/froggermain/FroggerMain.java
@@ -18,7 +18,8 @@ public class FroggerMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new Game("Frogger",600,400);
+        Game game = new Game("Frogger",600,400);
+        game.start();
     }
     
 }
--- src/froggermain/Game.java
+++ src/froggermain/Game.java
@@ -5,19 +5,76 @@
  */
 package froggermain;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Yasuki
  */
-public class Game {
+public class Game implements Runnable{
     
     private FroggerFrame frame1;
-    
     public int width, height;
+    public String title;
+    
+    private boolean running = false;
+    private Thread thread;
     
     public Game(String t, int w, int h){
+        title = t;
         width = w;
         height = h;
-        frame1 = new FroggerFrame(t, w, h);
+    }
+    
+    //initialize graphics of game
+    private void init(){
+        frame1 = new FroggerFrame(title, width, height);
+   
+    }
+    
+    //updates all variables, positions, etc
+    private void tick(){
+        
+    }
+    
+    //render(draw) everything on screen
+    private void render(){
+        
+        
+    }
+    
+    //called by start
+    public void run(){
+        init();
+        //game loop
+        while(running){
+            tick();
+            render(); 
+        }
+        stop();
+    }
+    
+    public synchronized void start(){
+        //prevents starting thread if thread already started
+        if(running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    
+    public synchronized void stop(){
+        //prevents stopping thread if thread already stopped
+        if(!running)
+            return;
+        running = false;
+        //prevents error, idk what it really does 
+        try {
+            thread.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
 }
