95f792051b1731eb91850807054a3f81243496eb
Yasuki <yasuki.wu.02@gmail.com>
2019-05-27 20:36:55 -0500

    test commit

--- src/froggermain/FroggerMain.java
+++ src/froggermain/FroggerMain.java
@@ -5,18 +5,28 @@
  */
 package froggermain;
 
+
+
 /**
  *
  * @author Yasuki Wu
  */
+import javax.swing.JFrame;
+import java.awt.GraphicsConfiguration;
 public class FroggerMain {
 
     /**
      * @param args the command line arguments
      */
-    
+    static GraphicsConfiguration gc;
     public static void main(String[] args) {
         // TODO code application logic here
+         JFrame frame= new JFrame(gc);	
+	 frame.setTitle("Frogger");
+	 frame.setSize(600, 400);
+	 frame.setLocation(200, 200);
+	 frame.setVisible(true);
+	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
 }
